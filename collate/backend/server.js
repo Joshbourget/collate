0b21ed170eddad1e7815937db85eded16732d0ff
Josh Bourget <joshbourget@Joshs-MacBook-Pro.local>
2023-07-08 15:32:21 -0500

    Worked on backend, connected to MongoDB database

---
+++ collate/backend/server.js
@@ -0,0 +1,21 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const connection = mongoose.connection;
+connection.once('open', () => {
+  console.log("MongoDB database connection established successfully");
+})
+
+app.listen(5001, () => {
+    console.log('Server is running on port 5001');
+});
